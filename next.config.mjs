a33329db889e7020fc8dd5dde6a6bc7602c56d3d
Femil Savaliya <femil@socialpilot.co>
2024-03-14 22:13:20 +0530

    diable webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Webpack 5 is enabled by default
+  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+  webpack5: false,
+};
 
 export default nextConfig;
